64db0e4f302139709b5ac2cc64ca7561a3b73761
Jacob Foard <jacob.foard@formation.ai>
2018-03-20 11:37:39 -0700

    Migrating to dep
    Signed-off-by: Jacob Foard <jacob.foard@formation.ai>

---
+++ vendor/github.com/jtblin/kube2iam/iam/iam.go
@@ -0,0 +1,111 @@
+package iam
+
+import (
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/karlseguin/ccache"
+)
+
+var cache = ccache.New(ccache.Configure())
+
+const (
+	maxSessNameLength = 64
+	ttl               = time.Minute * 15
+)
+
+// Client represents an IAM client.
+type Client struct {
+	BaseARN string
+}
+
+// Credentials represent the security Credentials response.
+type Credentials struct {
+	AccessKeyID     string `json:"AccessKeyId"`
+	Code            string
+	Expiration      string
+	LastUpdated     string
+	SecretAccessKey string
+	Token           string
+	Type            string
+}
+
+func getHash(text string) string {
+	h := fnv.New32a()
+	_, err := h.Write([]byte(text))
+	if err != nil {
+		return text
+	}
+	return fmt.Sprintf("%x", h.Sum32())
+}
+
+// GetInstanceIAMRole get instance IAM role from metadata service.
+func GetInstanceIAMRole() (string, error) {
+	sess, err := session.NewSession()
+	if err != nil {
+		return "", err
+	}
+	metadata := ec2metadata.New(sess)
+	if !metadata.Available() {
+		return "", errors.New("EC2 Metadata is not available, are you running on EC2?")
+	}
+	iamRole, err := metadata.GetMetadata("iam/security-credentials/")
+	if err != nil {
+		return "", err
+	}
+	if iamRole == "" || err != nil {
+		return "", errors.New("EC2 Metadata didn't returned any IAM Role")
+	}
+	return iamRole, nil
+}
+
+func sessionName(roleARN, remoteIP string) string {
+	idx := strings.LastIndex(roleARN, "/")
+	name := fmt.Sprintf("%s-%s", getHash(remoteIP), roleARN[idx+1:])
+	return fmt.Sprintf("%.[2]*[1]s", name, maxSessNameLength)
+}
+
+// AssumeRole returns an IAM role Credentials using AWS STS.
+func (iam *Client) AssumeRole(roleARN, remoteIP string) (*Credentials, error) {
+	item, err := cache.Fetch(roleARN, ttl, func() (interface{}, error) {
+		sess, err := session.NewSession()
+		if err != nil {
+			return nil, err
+		}
+		svc := sts.New(sess, &aws.Config{LogLevel: aws.LogLevel(2)})
+		resp, err := svc.AssumeRole(&sts.AssumeRoleInput{
+			DurationSeconds: aws.Int64(int64(ttl.Seconds() * 2)),
+			RoleArn:         aws.String(roleARN),
+			RoleSessionName: aws.String(sessionName(roleARN, remoteIP)),
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return &Credentials{
+			AccessKeyID:     *resp.Credentials.AccessKeyId,
+			Code:            "Success",
+			Expiration:      resp.Credentials.Expiration.Format("2006-01-02T15:04:05Z"),
+			LastUpdated:     time.Now().Format("2006-01-02T15:04:05Z"),
+			SecretAccessKey: *resp.Credentials.SecretAccessKey,
+			Token:           *resp.Credentials.SessionToken,
+			Type:            "AWS-HMAC",
+		}, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return item.Value().(*Credentials), nil
+}
+
+// NewClient returns a new IAM client.
+func NewClient(baseARN string) *Client {
+	return &Client{BaseARN: baseARN}
+}
---
+++ vendor/github.com/jtblin/kube2iam/server/server.go
@@ -0,0 +1,313 @@
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/cenk/backoff"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/jtblin/kube2iam"
+	"github.com/jtblin/kube2iam/iam"
+	"github.com/jtblin/kube2iam/k8s"
+	"github.com/jtblin/kube2iam/mappings"
+)
+
+const (
+	defaultAppPort           = "8181"
+	defaultCacheSyncAttempts = 10
+	defaultIAMRoleKey        = "iam.amazonaws.com/role"
+	defaultLogLevel          = "info"
+	defaultMaxElapsedTime    = 2 * time.Second
+	defaultMaxInterval       = 1 * time.Second
+	defaultMetadataAddress   = "169.254.169.254"
+	defaultNamespaceKey      = "iam.amazonaws.com/allowed-roles"
+)
+
+// Server encapsulates all of the parameters necessary for starting up
+// the server. These can either be set via command line or directly.
+type Server struct {
+	APIServer               string
+	APIToken                string
+	AppPort                 string
+	BaseRoleARN             string
+	DefaultIAMRole          string
+	IAMRoleKey              string
+	MetadataAddress         string
+	HostInterface           string
+	HostIP                  string
+	NamespaceKey            string
+	LogLevel                string
+	AddIPTablesRule         bool
+	AutoDiscoverBaseArn     bool
+	AutoDiscoverDefaultRole bool
+	Debug                   bool
+	Insecure                bool
+	NamespaceRestriction    bool
+	Verbose                 bool
+	Version                 bool
+	iam                     *iam.Client
+	k8s                     *k8s.Client
+	roleMapper              *mappings.RoleMapper
+	BackoffMaxElapsedTime   time.Duration
+	BackoffMaxInterval      time.Duration
+}
+
+type appHandler func(*log.Entry, http.ResponseWriter, *http.Request)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+// ServeHTTP implements the net/http server Handler interface
+// and recovers from panics.
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logger := log.WithFields(log.Fields{
+		"req.method": r.Method,
+		"req.path":   r.URL.Path,
+		"req.remote": parseRemoteAddr(r.RemoteAddr),
+	})
+	start := time.Now()
+	defer func() {
+		var err error
+		if rec := recover(); rec != nil {
+			switch t := rec.(type) {
+			case string:
+				err = errors.New(t)
+			case error:
+				err = t
+			default:
+				err = errors.New("Unknown error")
+			}
+			logger.WithField("res.status", http.StatusInternalServerError).
+				Errorf("PANIC error processing request: %+v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}()
+	rw := newResponseWriter(w)
+	fn(logger, rw, r)
+	if r.URL.Path != "/healthz" {
+		latency := time.Since(start)
+		logger.WithFields(log.Fields{"res.duration": latency.Nanoseconds(), "res.status": rw.statusCode}).
+			Infof("%s %s (%d) took %d ns", r.Method, r.URL.Path, rw.statusCode, latency.Nanoseconds())
+	}
+}
+
+func parseRemoteAddr(addr string) string {
+	n := strings.IndexByte(addr, ':')
+	if n <= 1 {
+		return ""
+	}
+	hostname := addr[0:n]
+	if net.ParseIP(hostname) == nil {
+		return ""
+	}
+	return hostname
+}
+
+func (s *Server) getRoleMapping(IP string) (*mappings.RoleMappingResult, error) {
+	var roleMapping *mappings.RoleMappingResult
+	var err error
+	operation := func() error {
+		roleMapping, err = s.roleMapper.GetRoleMapping(IP)
+		return err
+	}
+
+	expBackoff := backoff.NewExponentialBackOff()
+	expBackoff.MaxInterval = s.BackoffMaxInterval
+	expBackoff.MaxElapsedTime = s.BackoffMaxElapsedTime
+
+	err = backoff.Retry(operation, expBackoff)
+	if err != nil {
+		return nil, err
+	}
+
+	return roleMapping, nil
+}
+
+// HealthResponse represents a response for the health check.
+type HealthResponse struct {
+	HostIP     string `json:"hostIP"`
+	InstanceID string `json:"instanceId"`
+}
+
+func (s *Server) healthHandler(logger *log.Entry, w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get(fmt.Sprintf("http://%s/latest/meta-data/instance-id", s.MetadataAddress))
+	if err != nil {
+		log.Errorf("Error getting instance id %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Error getting instance id, got status: %+s", resp.Status)
+		log.Error(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	instanceID, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("Error reading response body %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	health := &HealthResponse{InstanceID: string(instanceID), HostIP: s.HostIP}
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(health); err != nil {
+		log.Errorf("Error sending json %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) debugStoreHandler(logger *log.Entry, w http.ResponseWriter, r *http.Request) {
+	o, err := json.Marshal(s.roleMapper.DumpDebugInfo())
+	if err != nil {
+		log.Errorf("Error converting debug map to json: %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	write(logger, w, string(o))
+}
+
+func (s *Server) securityCredentialsHandler(logger *log.Entry, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "EC2ws")
+	remoteIP := parseRemoteAddr(r.RemoteAddr)
+	roleMapping, err := s.getRoleMapping(remoteIP)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// If a base ARN has been supplied and this is not cross-account then
+	// return a simple role-name, otherwise return the full ARN
+	if s.iam.BaseARN != "" && strings.HasPrefix(roleMapping.Role, s.iam.BaseARN) {
+		write(logger, w, strings.TrimPrefix(roleMapping.Role, s.iam.BaseARN))
+		return
+	}
+	write(logger, w, roleMapping.Role)
+}
+
+func (s *Server) roleHandler(logger *log.Entry, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "EC2ws")
+	remoteIP := parseRemoteAddr(r.RemoteAddr)
+
+	roleMapping, err := s.getRoleMapping(remoteIP)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	roleLogger := logger.WithFields(log.Fields{
+		"pod.iam.role": roleMapping.Role,
+		"ns.name":      roleMapping.Namespace,
+	})
+
+	wantedRole := mux.Vars(r)["role"]
+	wantedRoleARN := s.iam.RoleARN(wantedRole)
+
+	if wantedRoleARN != roleMapping.Role {
+		roleLogger.WithField("params.iam.role", wantedRole).
+			Error("Invalid role: does not match annotated role")
+		http.Error(w, fmt.Sprintf("Invalid role %s", wantedRole), http.StatusForbidden)
+		return
+	}
+
+	credentials, err := s.iam.AssumeRole(wantedRoleARN, remoteIP)
+	if err != nil {
+		roleLogger.Errorf("Error assuming role %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(credentials); err != nil {
+		roleLogger.Errorf("Error sending json %+v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) reverseProxyHandler(logger *log.Entry, w http.ResponseWriter, r *http.Request) {
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host: s.MetadataAddress})
+	proxy.ServeHTTP(w, r)
+	logger.WithField("metadata.url", s.MetadataAddress).Debug("Proxy ec2 metadata request")
+}
+
+func write(logger *log.Entry, w http.ResponseWriter, s string) {
+	if _, err := w.Write([]byte(s)); err != nil {
+		logger.Errorf("Error writing response: %+v", err)
+	}
+}
+
+// Run runs the specified Server.
+func (s *Server) Run(host, token string, insecure bool) error {
+	k, err := k8s.NewClient(host, token, insecure)
+	if err != nil {
+		return err
+	}
+	s.k8s = k
+	s.iam = iam.NewClient(s.BaseRoleARN)
+	s.roleMapper = mappings.NewRoleMapper(s.IAMRoleKey, s.DefaultIAMRole, s.NamespaceRestriction, s.NamespaceKey, s.iam, s.k8s)
+	podSynched := s.k8s.WatchForPods(kube2iam.NewPodHandler(s.IAMRoleKey))
+	namespaceSynched := s.k8s.WatchForNamespaces(kube2iam.NewNamespaceHandler(s.NamespaceKey))
+
+	synced := false
+	for i := 0; i < defaultCacheSyncAttempts && !synced; i++ {
+		synced = cache.WaitForCacheSync(nil, podSynched, namespaceSynched)
+	}
+
+	if !synced {
+		log.Fatalf("Attempted to wait for caches to be synced for %d however it is not done.  Giving up.", defaultCacheSyncAttempts)
+	} else {
+		log.Debugln("Caches have been synced.  Proceeding with server.")
+	}
+
+	r := mux.NewRouter()
+
+	if s.Debug {
+		// This is a potential security risk if enabled in some clusters, hence the flag
+		r.Handle("/debug/store", appHandler(s.debugStoreHandler))
+	}
+	r.Handle("/{version}/meta-data/iam/security-credentials/", appHandler(s.securityCredentialsHandler))
+	r.Handle("/{version}/meta-data/iam/security-credentials/{role:.*}", appHandler(s.roleHandler))
+	r.Handle("/healthz", appHandler(s.healthHandler))
+	r.Handle("/{path:.*}", appHandler(s.reverseProxyHandler))
+
+	log.Infof("Listening on port %s", s.AppPort)
+	if err := http.ListenAndServe(":"+s.AppPort, r); err != nil {
+		log.Fatalf("Error creating http server: %+v", err)
+	}
+	return nil
+}
+
+// NewServer will create a new Server with default values.
+func NewServer() *Server {
+	return &Server{
+		AppPort:               defaultAppPort,
+		BackoffMaxElapsedTime: defaultMaxElapsedTime,
+		IAMRoleKey:            defaultIAMRoleKey,
+		BackoffMaxInterval:    defaultMaxInterval,
+		LogLevel:              defaultLogLevel,
+		MetadataAddress:       defaultMetadataAddress,
+		NamespaceKey:          defaultNamespaceKey,
+	}
+}
